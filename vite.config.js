f3f4f5aed8c590eadfec0a603bd53021441fa3a8
Ikram Syakir <ikramsyakir@gmail.com>
2026-03-06 12:55:20 +0800

    Update base URL in Vite configuration to root path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/jpnj-hsk-permit/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
